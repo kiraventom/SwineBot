76c563f4358ac5752a0d2b97baed9cb95d2cc402
Temirkanov Alikhan <kiraventom@gmail.com>
2025-12-07 23:17:49 +0300

    Fix group commands

--- Actions/Commands/Command.cs
+++ Actions/Commands/Command.cs
@@ -2,4 +2,11 @@ using Serilog;
 
 namespace SwineBot.Actions.Commands;
 
-public abstract class Command(ILogger logger, BotMessageSender sender) : UserAction(logger, sender);
+public abstract class Command(ILogger logger, BotMessageSender sender) : UserAction(logger, sender)
+{
+    public override bool IsMatch(string name)
+    {
+        var commandText = name.Substring(0, name.IndexOf('@'));
+        return base.IsMatch(commandText);
+    }
+}
